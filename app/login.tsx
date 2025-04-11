8bd2c5af3e4781c6a7246066da1b12efab0af686
Tayo <teeydigba@gmail.com>
2025-04-11 11:24:04 +0100

    completedly added form input, login page form and its validation

--- app/login.tsx
+++ app/login.tsx
@@ -1,14 +1,27 @@
 import { View } from "react-native";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
-import { AppText, PrimaryButton, TextField } from "@/components";
+import { PrimaryButton, TextField } from "@/components";
 import { useTheme } from "@/contexts";
+import { loginSchema } from "@/validators";
+
+type LoginInputType = z.infer<typeof loginSchema>;
 
 const LoginPage = () => {
   const { theme } = useTheme();
-  const { control, handleSubmit } = useForm();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputType>({
+    resolver: zodResolver(loginSchema),
+  });
 
-  const __handleSubmit = (data) => {};
+  const __handleSubmit = (data: LoginInputType) => {
+    console.log(data);
+  };
 
   return (
     <View
@@ -18,17 +31,20 @@ const LoginPage = () => {
         alignItems: "center",
         backgroundColor: theme.background,
         paddingHorizontal: 16,
+        gap: 30,
       }}
     >
       <TextField
         name="email"
         control={control}
         placeholder="Enter your email"
+        error={errors?.email?.message}
       />
       <TextField
         name="password"
         control={control}
         placeholder="Enter password"
+        error={errors?.password?.message}
       />
       <PrimaryButton title="Submit" onPress={handleSubmit(__handleSubmit)} />
     </View>
--- components/InputFields.tsx
+++ components/InputFields.tsx
@@ -1,14 +1,21 @@
-import { TextInput, TextInputProps } from "react-native";
+import { TextInput, TextInputProps, View } from "react-native";
 import { useController, Control } from "react-hook-form";
 
 import { useTheme } from "@/contexts";
+import { AppText } from "./AppText";
 
-type InputFieldProps = TextInputProps & {
+type InputFieldProps<T> = TextInputProps & {
   name: string;
-  control: Control;
+  control: Control<any, any, T>;
+  error: string | undefined;
 };
 
-export const TextField = ({ name, control, ...rest }: InputFieldProps) => {
+export const TextField = <T,>({
+  name,
+  control,
+  error,
+  ...rest
+}: InputFieldProps<T>) => {
   const { theme } = useTheme();
   const { field } = useController({
     control,
@@ -16,21 +23,30 @@ export const TextField = ({ name, control, ...rest }: InputFieldProps) => {
     name,
   });
   return (
-    <TextInput
-      {...rest}
-      value={field.value}
-      onChangeText={field.onChange}
+    <View
       style={{
-        paddingVertical: 10,
-        paddingHorizontal: 12,
-        borderWidth: 1,
-        height: 50,
         width: "100%",
-        marginBottom: 20,
-        borderColor: theme.border,
-        borderRadius: 5,
-        color: theme.text,
       }}
-    />
+    >
+      <TextInput
+        {...rest}
+        value={field.value}
+        onChangeText={field.onChange}
+        style={{
+          paddingVertical: 10,
+          paddingHorizontal: 12,
+          borderWidth: 1,
+          height: 50,
+          width: "100%",
+          marginBottom: 3,
+          borderColor: error ? "#cc0000" : theme.border,
+          borderRadius: 5,
+          color: theme.text,
+        }}
+      />
+      {!!error && (
+        <AppText title={error?.toString()} color="#cc0000" size="small" />
+      )}
+    </View>
   );
 };
